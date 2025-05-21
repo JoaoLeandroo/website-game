7deeccab193139cd0ffcf9ada076bda68eba6310
joaoleandro <new.joaoleandro@gmail.com>
2025-05-21 18:59:16 -0300

    feat: instalação shadCn, organização navbar

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,34 +1,50 @@
 import Image from "next/image";
 import ContainerMain from "../others/ContainerMain";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 const NavBar = () => {
-    return ( 
-        <header className="bg-blue-800 sticky top-0 z-50 text-textBase font-semibold shadow">
-            <ContainerMain>
-                <nav className="w-full h-14 flex justify-between items-center">
-                    {/* logo */}
-                    <Link href={"/"}>
-                        <Image src={"/assets/RoseOnline_Logo.png"} width={70} height={70} alt="Logo Rose Online Brasil"/>
-                    </Link>
+  return (
+    <header className="bg-blue-800 sticky top-0 z-50 text-textBase shadow">
+      <ContainerMain>
+        <nav className="w-full h-16 flex justify-between items-center">
+          {/* logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/assets/RoseOnline_Logo.png"}
+              width={70}
+              height={70}
+              alt="Logo Rose Online Brasil"
+            />
+          </Link>
 
-                    {/* conteudo */}
-                    <div className="flex gap-x-2">
-                        <Link href={"#"}>Guides</Link>
-                        <Link href={"#"}>Donate</Link>
-                        <Link href={"/download"}>Download</Link>
+          {/* conteudo */}
+          <div className="flex gap-x-4 text-md items-center justify-center">
+            <Link href={"#"}>Guides</Link>
+            <Link href={"#"}>Freature</Link>
+            <Link href={"#"}>Item Mall</Link>
+            <Link href={"#"}>Comunidade</Link>
+            <Link href={"#"}>Donate</Link>
+            <Link
+              href={"/download"}
+            >
+              <Button className="bg-[#FFEB3B] hover:bg-[#ffeb3bdb] text-[#1A1A1A] cursor-custom-hand">Download</Button>
+            </Link>
+          </div>
 
-                    </div>
+          {/* auth */}
+          <div className="flex gap-x-2">
+            <Link href={"/session"}>
+              <Button className="cursor-custom-hand">Login</Button>
+            </Link>
+            <Link href={"/register"}>
+              <Button className="cursor-custom-hand">Register</Button>
+            </Link>
+          </div>
+        </nav>
+      </ContainerMain>
+    </header>
+  );
+};
 
-                    {/* auth */}
-                    <div className="flex gap-x-2">
-                        <Link href={"#"}>Login</Link>
-                        <Link href={"#"}>Register</Link>
-                    </div>
-                </nav>
-            </ContainerMain>
-        </header>
-     );
-}
- 
 export default NavBar;
