2620e51cc196810e694d447967de472948546d7a
joaoleandro <new.joaoleandro@gmail.com>
2025-05-26 09:57:05 -0300

    feat: pagina de refines

---
+++ src/app/freatures/refines/page.tsx
@@ -0,0 +1,71 @@
+import SectionContent from "@/components/others/SectionContent";
+import { TableRefines } from "@/components/others/TableRefines";
+import { BreadcrumbPages } from "@/components/others/BreadcrumbPages";
+import Image from "next/image";
+import AnimationsScroll from "@/components/others/AnimationsScroll";
+
+const PageRefines = () => {
+  return (
+    <SectionContent>
+      <section className="flex flex-col px-4 md:px-10">
+        <div className="mt-5">
+          <BreadcrumbPages namePage="Refines" />
+        </div>
+
+        <div>
+          <h1 className="gradient-text-yellow font-bungee text-3xl drop-shadow-primary mt-8">
+            Refines
+          </h1>
+          <p className="text-desc mt-2 mb-5">
+            Quer dar uma nova vida àquela arma antiga? Seu machado está gasto ou
+            sua espada está cega? Talvez você queira fazer sua armadura brilhar
+            mais? Refine seus itens! O processo de refinamento aumenta os
+            atributos de suas armas e armaduras, tornando você mais poderoso em
+            batalha. Embora haja alguns riscos envolvidos, os benefícios superam
+            as desvantagens.
+          </p>
+
+          <span className="font-bold text-xl text-tittle">
+            Preparando-se para o refinamento
+          </span>
+          <p className="text-desc">
+            Escolha o item que deseja refinar e reúna todos os materiais
+            necessários. Para refinar um item acima de +3, você precisará de
+            Materiais Químicos, Materiais de Refinamento e Corações. A
+            quantidade de materiais varia dependendo do nível de refinamento e
+            do tipo de item.
+          </p>
+        </div>
+        <picture>
+          <Image
+            src={"/freatures/refines.avif"}
+            width={1280}
+            height={300}
+            alt="personagem refinando"
+            className="rounded shadow mt-6"
+          />
+        </picture>
+
+        <span className="font-bold text-xl text-tittle mt-8">
+          Materiais Químicos
+        </span>
+        <p className="text-desc">
+          Eles são essenciais para o processo de refinamento, qualidade do
+          material utilizado afeta a taxa de sucesso. Materiais de maior
+          qualidade aumentam suas chances de sucesso no refino.
+        </p>
+
+        <AnimationsScroll>
+          <div className="flex flex-col mb-20">
+            <span className="font-bold text-xl text-tittle mt-12">
+              Tabela de Refinamento
+            </span>
+            <TableRefines />
+          </div>
+        </AnimationsScroll>
+      </section>
+    </SectionContent>
+  );
+};
+
+export default PageRefines;
---
+++ src/components/others/TableRefines.tsx
@@ -0,0 +1,92 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const invoices = [
+  {
+    invoice: "+1",
+    paymentStatus: "3-5x",
+    totalAmount: "",
+    paymentMethod: "5-7x Low essence",
+  },
+  {
+    invoice: "+2",
+    paymentStatus: "4-7x",
+    totalAmount: "",
+    paymentMethod: "7-9x Essence",
+  },
+  {
+    invoice: "+3",
+    paymentStatus: "5-8x",
+    totalAmount: "1x Black heart",
+    paymentMethod: "9-12x Low ether",
+  },
+  {
+    invoice: "+4",
+    paymentStatus: "6-9x",
+    totalAmount: "1x Green heart",
+    paymentMethod: "11-15x Ether",
+  },
+  {
+    invoice: "+5",
+    paymentStatus: "7-10x",
+    totalAmount: "1-2x Blue heart",
+    paymentMethod: "13-18x High ether",
+  },
+  {
+    invoice: "+6",
+    paymentStatus: "8-12x",
+    totalAmount: "2x Pink heart",
+    paymentMethod: "15-22x Elixir",
+  },
+  {
+    invoice: "+7",
+    paymentStatus: "9-14x",
+    totalAmount: "2x Red heart",
+    paymentMethod: "18-25x hime",
+  },
+  ,
+  {
+    invoice: "+8",
+    paymentStatus: "10-16x",
+    totalAmount: "2-3x Golden heart",
+    paymentMethod: "22-28x Low enthiric",
+  },
+  ,
+  {
+    invoice: "+9",
+    paymentStatus: "12-18x",
+    totalAmount: "3x White heart",
+    paymentMethod: "25-30x Enthiric",
+  },
+];
+
+export function TableRefines() {
+  return (
+    <Table className="text-desc">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Level</TableHead>
+          <TableHead>Quimicos</TableHead>
+          <TableHead>Materiais de refinamento</TableHead>
+          <TableHead className="text-left">Hearts</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice?.invoice}>
+            <TableCell className="font-medium">{invoice?.invoice}</TableCell>
+            <TableCell>{invoice?.paymentStatus}</TableCell>
+            <TableCell>{invoice?.paymentMethod}</TableCell>
+            <TableCell className="text-left">{invoice?.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
