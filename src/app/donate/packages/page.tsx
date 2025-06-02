0605216b6c23ae3443bec1466869420a32e54db6
joaoleandro <new.joaoleandro@gmail.com>
2025-06-02 14:17:51 -0300

    pacotes de rc

--- src/app/donate/packages/page.tsx
+++ src/app/donate/packages/page.tsx
@@ -1,13 +1,33 @@
 import SectionContent from "@/components/others/SectionContent";
 import CardPack from "../_components/CardPack";
+import { TriangleAlert } from "lucide-react";
+import { BreadcrumbPages } from "@/components/others/BreadcrumbPages";
 
 const Packages = () => {
   return (
     <SectionContent>
       <section className="flex flex-col">
-        <div className="w-full bg-blue-300">teste</div>
+        <div className="mt-5">
+          <BreadcrumbPages namePage="Sistema de doação" />
+        </div>
+        <div className="w-full bg-yellow-400 p-2 rounded">
+          <p className="text-desc text-xs">
+            <TriangleAlert size={20} />
+            Bem vindo ao sistema de doações. Lembramos que a doação é
+            facultativa e não reembolsável e o brinde para os doadores podem ser
+            trocados por itens do jogo. Nosso sistema está totalmente
+            automatizado, portanto, assim que você fizer uma doação, você
+            receberá uma compensação por nos ajudar, que chamamos de RoseCash.{" "}
+            <br />
+            ATENÇÃO! O Cash será creditado na conta que você está logado no
+            momento e não poderá ser transferido para outra conta. Caso deseje
+            que o cash vá para outra conta, faça o login aqui no site novamente
+            com a conta desejada. Se quiser doar e receber cash para 2 ou mais
+            contas diferentes, logue com cada uma e faça a doação separadamente.
+          </p>
+        </div>
 
-        <div className="flex gap-x-3 gap-y-4 mt-10 flex-wrap items-center justify-center">
+        <div className="flex gap-x-3 gap-y-4 mt-5 flex-wrap items-center justify-center">
           <CardPack
             namePack="Opala"
             amountRc={"100"}
